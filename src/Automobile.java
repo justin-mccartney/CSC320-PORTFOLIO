0b4479f39f72e0700b7cb2118b2d1971b6801c7d
justin-mccartney <justin.mccartney@csuglobal.edu>
2024-04-01 02:06:35 -0500

    Version 1...

---
+++ src/Automobile.java
@@ -0,0 +1,42 @@
+public class Automobile {
+    private String make;
+    private String model;
+    private String color;
+    private int year;
+    private int mileage;
+
+    public Automobile(String make, String model, String color, int year, int mileage) {
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.mileage = mileage;
+    }
+    public String addVehicle(String make, String model, String color, int year, int mileage) {
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.mileage = mileage;
+        return "Vehicle added successfully!";
+    }
+    public String[] vehicleInformation() {
+        return new String[]{make, model, color, String.valueOf(year), String.valueOf(mileage)};
+    }
+    public String removeVehicle() {
+        this.make = null;
+        this.model = null;
+        this.color = null;
+        this.year = 0;
+        this.mileage = 0;
+        return "Vehicle removed successfully!";
+    }
+    public String updateVehicle(String make, String model, String color, int year, int mileage) {
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.mileage = mileage;
+        return "Vehicle updated successfully!";
+    }
+}
